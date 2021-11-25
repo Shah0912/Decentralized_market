a6a4bf27e0df4f6c295afc0eec779e69dc1b0e24
"Shah0912" <"adi.shah0912@gmail.com">
2021-11-25 19:14:47 +0530

    added functionality to create auction, bid on auctions

---
+++ contracts/AuctionRepository.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./DeedRepository.sol";
+
+contract AuctionRepository {
+    
+    // Array with all auctions
+    Auction[] public auctions;
+
+    // Mapping from auction index to user bids
+    mapping(uint256 => Bid[]) public auctionBids;
+
+    // Mapping from owner to a list of owned auctions
+    mapping(address => uint[]) public auctionOwner;
+
+    // Bid struct to hold bidder and amount
+    struct Bid {
+        address payable from;
+        uint256 amount;
+    }
+
+    // Auction struct which holds all the required info
+    struct Auction {
+        string name;
+        uint256 startBlock;
+        uint256 endBlock;
+        uint256 startPrice;
+        string metadata;
+        uint256 deedId;
+        address deedRepositoryAddress;
+        address owner;
+        bool active;
+        bool finalized;
+    }
+    
+    modifier isOwner(uint _auctionId) {
+        require(auctions[_auctionId].owner == msg.sender);
+        _;
+    }
+    
+    modifier contractHasDeedApproval(address _deedRepositoryAddress, uint256 _deedId) {
+        address deedApproval = DeedRepository(_deedRepositoryAddress).getApproved(_deedId);
+        require(deedApproval == address(this));
+        _;
+    }
+
+    modifier auctionIsActive(uint _auctionId) {
+        require(auctions.length > _auctionId);
+        require(auctions[_auctionId].active == true);
+        require(auctions[_auctionId].finalized == false);
+        uint current_block = getCurrentBlock();
+        require(current_block >= auctions[_auctionId].startBlock);
+        require(current_block < auctions[_auctionId].endBlock);
+        _;
+    }
+    
+    function createAuction(address _deedRepositoryAddress, uint256 _deedId, string memory _auctionTitle, string memory _metadata, uint256 _startPrice, uint256 _startBlock, uint256 _endBlock) public contractHasDeedApproval(_deedRepositoryAddress, _deedId) returns(bool) {
+        uint auctionId = auctions.length;
+        Auction memory newAuction;
+        newAuction.name = _auctionTitle;
+        newAuction.startBlock = _startBlock;
+        newAuction.endBlock = _endBlock;
+        // newAuction.startTime = block.timestamp;
+        newAuction.startPrice = _startPrice;
+        newAuction.metadata = _metadata;
+        newAuction.deedId = _deedId;
+        newAuction.deedRepositoryAddress = _deedRepositoryAddress;
+        newAuction.owner = msg.sender;
+        newAuction.active = true;
+        newAuction.finalized = false;
+
+        auctions.push(newAuction);        
+        auctionOwner[msg.sender].push(auctionId);
+        
+        // emit AuctionCreated(msg.sender, auctionId);
+        return true;
+    }
+    
+    function getCurrentBlock() public view returns(uint256) {
+        return block.number;
+    }
+
+    function bid(uint _auctionId) public payable auctionIsActive(_auctionId) {
+        uint256 ethAmountSent = msg.value;
+
+        Auction memory myAuction = auctions[_auctionId];
+        require(msg.sender != auctions[_auctionId].owner);
+
+        uint bidsLength = auctionBids[_auctionId].length;
+        uint256 tempAmount = myAuction.startPrice;
+        Bid memory lastBid;
+
+        if(bidsLength > 0) {
+            lastBid = auctionBids[_auctionId][bidsLength - 1];
+            tempAmount = lastBid.amount;
+        }
+        
+        if(ethAmountSent < tempAmount)  revert();
+
+        if(bidsLength > 0) {
+            if(!payable(lastBid.from).send(lastBid.amount)) revert();
+        }
+
+        Bid memory newBid;
+        newBid.from = payable(msg.sender);
+        newBid.amount = ethAmountSent;
+        auctionBids[_auctionId].push(newBid);
+
+    }
+
+    
+    function cancelAuction(uint _auctionId) public isOwner(_auctionId) {
+        Auction memory myAuction = auctions[_auctionId];
+        uint bidsLength = auctionBids[_auctionId].length;
+
+        // if there are bids refund the last bid
+        if( bidsLength > 0 ) {
+            Bid memory lastBid = auctionBids[_auctionId][bidsLength - 1];
+            if(!lastBid.from.send(lastBid.amount)) {
+                revert();
+            }
+        }
+        
+        // remove this after implementing transfer of money
+        auctions[_auctionId].active = false;
+
+        // approve and transfer from this contract to auction owner
+        // if(approveAndTransfer(address(this), myAuction.owner, myAuction.deedRepositoryAddress, myAuction.deedId)){
+        //     auctions[_auctionId].active = false;
+        //     emit AuctionCanceled(msg.sender, _auctionId);
+        // }
+    }
+    
+}
---
+++ contracts/DeedRepository.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+
+
+
+/**
+ * @title Repository of ERC721 Deeds
+ * This contract contains the list of deeds registered by users.
+ * This is a demo to show how tokens(deeds) can be mint and add to the repository
+ */
+contract DeedRepository is ERC721, Ownable {
+    
+    using Strings for uint256;
+    
+    // Optional mapping for token URIs
+    mapping (uint256 => string) private _tokenURIs;
+
+    // Base URI
+    string private _baseURIextended;
+
+
+    /**
+    * @dev Created a DeedRepository with a name and symbol
+    * @param _name string represents the name of the repository
+    * @param _symbol string represents the symbol of the repositor
+    */
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
+    
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+    
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+    
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+    
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+        
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        return string(abi.encodePacked(base, tokenId.toString()));
+    }
+    
+
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string memory tokenURI_
+    ) external onlyOwner() {
+        _mint(_to, _tokenId);
+        _setTokenURI(_tokenId, tokenURI_);
+    }
+    
+    
+    
+    
+    
+    
+
+
+    
+    
+    /**
+    * @dev Public function to register a new deed
+    * @dev Call the ERC721Token Minter
+    * @param _tokenId uint256 represents a specific deed
+    * @param _uri string containing metadata/uri
+    */
+    function registerDeed(uint256 _tokenId, string memory _uri) public {
+        _mint(msg.sender, _tokenId);
+        addDeedMetadata(_tokenId, _uri);
+        // _mint(msg.sender, _tokenId, _uri);
+        emit DeedRegistered(msg.sender, _tokenId);
+    }
+    
+
+    /**
+    * @dev Public function to add metadata to a deed
+    * @param _tokenId represents a specific deed
+    * @param _uri text which describes the characteristics of a given deed
+    * @return whether the deed metadata was added to the repository
+    */
+    function addDeedMetadata(uint256 _tokenId, string memory _uri) public returns(bool){
+        _setTokenURI(_tokenId, _uri);
+        return true;
+    }
+
+    /**
+    * @dev Event is triggered if deed/token is registered
+    * @param _by address of the registrar
+    * @param _tokenId uint256 represents a specific deed
+    */
+    event DeedRegistered(address _by, uint256 _tokenId);
+    
+    
+    
+    
+}
