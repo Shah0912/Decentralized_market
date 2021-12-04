e6ffc747df91ad9f0c24f20e1171799499a7e9f9
MMAP <mehdi.patel@gmail.com>
2021-12-04 20:11:21 +0400

    created backend and moved previously created contracts to contracts folder

--- contracts/DeedRepository.sol
+++ contracts/DeedRepository.sol
@@ -2,9 +2,10 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 
 
