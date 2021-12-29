e8e88b1b24b963fb006d9cf90884ea5593fe2994
Sarang Shekokar <shekokarsarang@gmail.com>
2021-12-29 10:58:19 +0530

    working sample without metamask

---
+++ sample_code/frontend/src/config.js
@@ -0,0 +1,23 @@
+var DeedRepository = require('./contracts/DeedRepository')
+var AuctionRepository = require('./contracts/AuctionRepository')
+
+module.exports = {
+    JSONRPC_ENDPOINT: 'http://52.59.238.144:8545',
+    JSONRPC_WS_ENDPOINT: 'ws://52.59.238.144:8546', //'ws://52.59.238.144:8546',
+    BZZ_ENDPOINT: 'http://52.59.238.144:8500',
+    SHH_ENDPOINT: 'ws://52.59.238.144:8546',
+
+    DEEDREPOSITORY_ADDRESS: '0x751ebd3eC3E76307aB048Fd01573762c2ca1bb97',
+    AUCTIONREPOSITORY_ADDRESS: '0xb2f6A43d20cEF00Bda2e999884D4b16Cbf8A6F64',
+
+    DEEDREPOSITORY_ABI: DeedRepository.abi,
+    AUCTIONREPOSITORY_ABI: AuctionRepository.abi,
+
+    GAS_AMOUNT: 500000,
+
+    //whisper settings
+    WHISPER_SHARED_KEY: '0x8bda3abeb454847b515fa9b404cede50b1cc63cfdeddd4999d074284b4c21e15'
+
+}
+
+// web3.eth.sendTransaction({from: web3.eth.accounts[0], to: "0x6f0023D1CFe5A7A56F96e61E0169B775Ac97f90E" , value: web3.toWei(1, 'ether'), gasLimit: 21000, gasPrice: 20000000000})
